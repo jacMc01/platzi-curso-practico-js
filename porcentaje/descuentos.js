187b74d12a4549538b023aa2c412f698acb728b9
Jacqueline Mckay H <jacquelinemc@outlook.es>
2022-01-21 12:59:24 -0600

    Se agrego otra pagina para descuentos, se hizo el html y el js, sin diseño

--- porcentaje/descuentos.js
+++ porcentaje/descuentos.js
@@ -6,6 +6,19 @@ function precio_final(precio_original, descuento){
   return precio_con_descuento;
 }
 
+function on_click_button_price_discount(){
+  const input_price = document.getElementById("input_price");
+  const price_value = input_price.value;
+
+  const input_discount = document.getElementById("input_discount");
+  const discount_value = input_discount.value;  
+  
+  const precio_con_descuento = precio_final(price_value, discount_value);
+
+  //aqui agrego el texto del precio final que paga el cliente.
+  const result_p = document.getElementById("result_price");
+  result_p.innerText = "El precio final a pagar es: " + precio_con_descuento;
+}
 
 
 
